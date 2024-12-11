4cda9c5e52f55fe140f90112e98ff66c48de59b5
Vikram Vasudevan <vikram.vasudevan@ekahaa.com>
2024-12-11 10:20:49 +0530

    Added ragam and pitch selection

--- app/src/main/java/com/example/swaramala/AvailableSwaramsFragment.kt
+++ app/src/main/java/com/example/swaramala/AvailableSwaramsFragment.kt
@@ -1,13 +1,18 @@
 package com.example.swaramala
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.GridView
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
+
 
 class AvailableSwaramsFragment : Fragment() {
     private lateinit var swaramGrid: GridView
@@ -16,6 +21,33 @@ class AvailableSwaramsFragment : Fragment() {
     }
 
     private val availableSwaramsViewModel: AvailableSwaramsViewModel by activityViewModels()
+
+    private fun populateDropDownRagam(myFragmentView : View){
+        val items = arrayOf("Mohanam", "Thodi", "Sahana")
+        val dropDownRagam = myFragmentView.findViewById<Spinner>(R.id.dropDownRagam)
+        val adapter = ArrayAdapter(myFragmentView.context, android.R.layout.simple_spinner_dropdown_item, items)
+        adapter.setDropDownViewResource(R.layout.spinner_style);
+        dropDownRagam.adapter = adapter
+        dropDownRagam.setSelection(0, true)
+        val view: View = dropDownRagam.getSelectedView()
+        (view as TextView).setTextColor(Color.WHITE)
+
+        dropDownRagam.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                // Change the selected item's text color
+                (view as TextView).setTextColor(Color.WHITE)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +62,8 @@ class AvailableSwaramsFragment : Fragment() {
         swaramGrid = myFragmentView.findViewById(R.id.swaramGrid)
         swaramGrid.adapter = availableSwaramsViewModel.getList()
             ?.let { SwaramAdapter(myFragmentView.context, it) }
+
+        populateDropDownRagam(myFragmentView)
         return myFragmentView
     }
 }
